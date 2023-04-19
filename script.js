82b85a6b997aaa4d2362506741046db7c8ea7d7d
kirube1992 <kirubelaynalem@gmail.com>
2023-04-19 15:09:29 -0300

    Add js file

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const hamburguer = document.querySelector(".breadcrumb");
-const Menu = document.querySelector(".desktop-nav");
-const main = document.querySelector("#portfolio");
+const hamburguer = document.querySelector('.breadcrumb');
+const Menu = document.querySelector('.desktop-nav');
+const main = document.querySelector('#portfolio');
 
-hamburguer.addEventListener("click", () => {
-  hamburguer.classList.toggle("active");
-  Menu.classList.toggle("active");
-  main.classList.toggle("blurred");
+hamburguer.addEventListener('click', () => {
+  hamburguer.classList.toggle('active');
+  Menu.classList.toggle('active');
+  main.classList.toggle('blurred');
 });
 
-document.querySelectorAll(".nav-link").forEach((n) => n.addEventListener("click", () => {
-  hamburguer.classList.toggle("active");
-  Menu.classList.toggle("active");
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburguer.classList.toggle('active');
+  Menu.classList.toggle('active');
 }));
