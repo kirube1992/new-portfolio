6aad55c774b4446d325890b31796a904e6d75d34
kirube1992 <kirubelaynalem@gmail.com>
2023-04-22 15:10:30 -0300

    Add js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const projects = [
        industry. Lorem Ipsum has been the industry's standard dummy text ever 
        since the 1500s, when an unknown printer took a galley of type and 
        scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing 
-       and typesetting industry.\</br></br>
+       and typesetting industry.\n</br></br>
        Lorem Ipsum has been the industry's standard dummy text ever since 
        the 1500s, when an unknown printer took a galley of type and scrambled it 
        1960s with the releorem Ipsum is simply dummy text of the printing and 
